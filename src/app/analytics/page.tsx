6096592ef6c31334b5210690dffb76e32598207c
Parth Chheda <parth.h.chheda@gmail.com>
2025-08-09 22:37:49 -0700

    feat: added breadcrumb for everything

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,5 +1,30 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
 const Analytics = ({}) => {
-  return <div>Analytics</div>;
+  return (
+    <div className="p-6 space-y-6">
+      {/** Breadcrumb */}
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/menu">Menu</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/analytics" className="font-semibold">
+              Analytics
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
 };
 
 export default Analytics;
--- src/app/applications/page.tsx
+++ src/app/applications/page.tsx
@@ -1,5 +1,30 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
 const Applications = ({}) => {
-  return <div>Applications</div>;
+  return (
+    <div className="p-6 space-y-6">
+      {/** Breadcrumb */}
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/menu">Menu</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/applications" className="font-semibold">
+              Applications
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
 };
 
 export default Applications;
