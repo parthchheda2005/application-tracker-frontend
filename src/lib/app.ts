e9213843f2373b83d2a9dbe83bb1c31cd8919122
Parth Chheda <parth.h.chheda@gmail.com>
2025-08-13 09:50:01 -0700

    fixed env variable naming

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
 });
 
 api.interceptors.request.use((config) => {
